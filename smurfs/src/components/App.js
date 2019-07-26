6a1e8ed23270182d485080302390719b9f8af657
unknown <samantha.c.egge@gmail.com>
2019-07-26 13:22:18 -0700

    Added functionality to display smurfs to page.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -4,25 +4,32 @@ import axios from 'axios'
 import { SmurfContext } from "../contexts/SmurfContext";
 
 import "./App.css";
+import Smurfs from './Smurf/Smurf'
 
 const App = (props) => {
-  const [smurfs, setSmurfs] = useState()
+  const [smurfs, setSmurfs] = useState([])
 
   useEffect(() => {
     axios
-      .get(`https://localhost:3333/api/smurfs`)
+      .get(`http://localhost:3333/smurfs`)
       .then(response => {
-        setSmurfs(response)
+        setSmurfs(response.data)
         console.log('this is the axios response : ', response)
       })
       .catch(error => console.log(error.response))
   },[])
 
+  if (smurfs.length < 1 ) {
+    return <h1>Loading . . .</h1>
+  }
+
   return (
     <SmurfContext.Provider>
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        {/* {smurfs.name} */}
+        {smurfs.map(smurf => (
+          <Smurfs smurf={smurf} />
+        ))}
       </div>
     </SmurfContext.Provider>
   );
