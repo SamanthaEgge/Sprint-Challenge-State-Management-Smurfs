d9415ed548c69928794f735571c6367073e0e39c
unknown <samantha.c.egge@gmail.com>
2019-07-26 13:10:10 -0700

    Initialized project, set up context Store. Testing axios calls against server.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,32 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
+import { SmurfContext } from "../contexts/SmurfContext";
+
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
+
+const App = (props) => {
+  const [smurfs, setSmurfs] = useState()
+
+  useEffect(() => {
+    axios
+      .get(`https://localhost:3333/api/smurfs`)
+      .then(response => {
+        setSmurfs(response)
+        console.log('this is the axios response : ', response)
+      })
+      .catch(error => console.log(error.response))
+  },[])
+
+  return (
+    <SmurfContext.Provider>
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {/* {smurfs.name} */}
       </div>
-    );
-  }
+    </SmurfContext.Provider>
+  );
 }
+
 
 export default App;
